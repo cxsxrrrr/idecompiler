a0cfe0aade777227c1c43861bb0ab556b5aab9a0
cxsxrrrr <xcesaramoranb@gmail.com>
2024-09-17 23:16:51 -0400

    ultimo lo juro

---
+++ IDEgf/src/components/sidebar.jsx
@@ -0,0 +1,22 @@
+
+function Sidebar() {
+  
+    return (
+      <>
+        <div className="sidebar">
+          <div className="sidebar-header">
+            <h3>IDEgf</h3>
+          </div>
+          <div className="sidebar-content">
+            <ul>
+              <li>File</li>
+              <li>Edit</li>
+              <li>View</li>
+              <li>Help</li>
+            </ul>
+          </div>
+        </div>
+      </>
+    )
+  }
+export default Sidebar
--- IDEgf/src/main.jsx
+++ IDEgf/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import Sidebar from '../components/sidebar.jsx'
+import Sidebar from './components/sidebar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
