0210e86b0ca0a0c6eca7a80cafba219c2c17bb72
cxsxrrrr <xcesaramoranb@gmail.com>
2024-09-17 23:04:54 -0400

    first first commit

---
+++ IDEgf/components/sidebar.jsx
@@ -0,0 +1,3 @@
+
+alert("Hola")
+export default Sidebar
--- IDEgf/src/main.jsx
+++ IDEgf/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Sidebar from '../components/sidebar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Sidebar />
   </StrictMode>,
 )
