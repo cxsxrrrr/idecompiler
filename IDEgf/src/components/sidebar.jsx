90c34753eccbe89bc0166a809dbc8ae63dbf4ff2
Sunlart <luisibp0410@gmail.com>
2024-09-29 04:07:47 -0400

    botones maricos que no hacen un coño. Despiertame sesar para seguir, me duele la espalda y estoy cansado

--- IDEgf/src/components/sidebar.jsx
+++ IDEgf/src/components/sidebar.jsx
@@ -1,20 +1,38 @@
-import './sidebar.css'
+import './sidebar.css';
+import files from '../assets/icons/files.png';
+import IA from '../assets/icons/IA.png';
+import run from '../assets/icons/run.png';
+import Save from '../assets/icons/Save.png';
 function Sidebar() {
-  
-    return (
-      <>
+  return (
+    <>
+      <aside className="sidebar">
+        <div className="navigation">
+          <div className="controls">
+            <div className="close" />
+            <div className="minimize" />
+            <div className="fullscreen" />
+          </div>
+          <div className="text-wrapper">Editor</div>
+        </div>
 
-    <aside className="sidebar">  
-       <div className="navigation">
-      <div className="controls">
-        <div className="close" />
-        <div className="minimize" />
-        <div className="fullscreen" />
-      </div>
-      <div className="text-wrapper">Editor</div>
-    </div></aside>
-    
-      </>
-    )
-  }
-export default Sidebar
+        <div className="sidebar-buttons">
+          <div className="sidebar-button" title="Carpetas">
+            <img src= {files} alt="Carpetas" />
+          </div>
+          <div className="sidebar-button" title="Guardado">
+          <img src= {Save} alt="Guardado" />
+          </div>
+          <div className="sidebar-button" title="IA">
+          <img src= {IA} alt="IA" />
+          </div>
+          <div className="sidebar-button" title="Ejecutar">
+          <img src= {run} alt="Ejecutar" />
+          </div>
+        </div>
+      </aside>
+    </>
+  );
+}
+
+export default Sidebar;
