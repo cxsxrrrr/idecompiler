e552e586bb99b1ee61a8b142985e07d95d09ee8c
cxsxrrrr <camoran@urbe.edu.ve>
2024-09-30 09:08:11 -0400

    Añadiendo app.py al control de versiones

---
+++ IDEgf/backend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import os
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app) 
+
+# Define el directorio donde se guardarán los archivos
+FILES_DIRECTORY = 'backend/files'
+
+# Asegúrate de que el directorio exista
+os.makedirs(FILES_DIRECTORY, exist_ok=True)
+
+@app.route('/create-file', methods=['POST'])
+def create_file():
+    data = request.get_json()
+    file_name = data.get('fileName')
+
+    if not file_name:
+        return jsonify({"error": "El nombre del archivo es requerido."}), 400
+
+    # Ya no verificamos la extensión del archivo
+    file_path = os.path.join(FILES_DIRECTORY, file_name)
+
+    try:
+        # Crea el archivo en blanco con el nombre proporcionado
+        with open(file_path, 'w') as f:
+            f.write("")  # Escribe contenido inicial si es necesario
+        return jsonify({"message": f"Archivo '{file_name}' creado exitosamente."}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
